11e01375a8cb924b59123e8736b6caa08b0fb948
alex <tgh88@mail.ru>
2019-02-13 13:17:55 +0300

    add validates

---
+++ app/models/user.rb
@@ -0,0 +1,46 @@
+require 'openssl'
+
+class User < ApplicationRecord
+  
+  attr_accessor :password
+
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+  validates :email, email_format: { message: "doesn't look like an email address" }
+  validates :username, :length => { :maximum => 40 }
+  validates_format_of :username, :with => /\A[a-z\_0-9]*\z/i
+
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      self.password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
+          password, password_salt, ITERATIONS, DIGEST.length, DIGEST))
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+
+    if user.present? && user.password_hash == User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
+end
