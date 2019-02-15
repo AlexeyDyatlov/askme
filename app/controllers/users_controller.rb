bc0e27ab4d90bad87aadb84fe9f84e6f2d697c58
Alexey <tgh88@mail.ru>
2019-02-16 01:53:06 +0300

    Views refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,38 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+      id: 1,
+      name: 'Alexey',
+      username: 'lucky',
+      avatar_url: 'https://pp.userapi.com/c849224/v849224224/7ee25/RHxvp-sdN38.jpg?ava=1'
+    ),
+    User.new(
+      id: 2,
+      name: 'Sasha',
+      username: 'kozlik')
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Привет, Володя!"
+    @user = User.new(
+      name: "Vasya",
+      username: 'vasya',
+      avatar_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTZ-J4K3bUmRbnVyopYQETMcSnIO2G1vmeTCGbgKs6hyO8xhLFVw')
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('15.02.2019')),
+      Question.new(
+        text: 'В чем смысл жизни?', created_at: Date.parse('15.02.2019')
+      )
+    ]
+
+    @new_question = Question.new
   end
 end
