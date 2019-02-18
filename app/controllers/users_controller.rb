a35a2a57072c68aad0cbc1e3421d7561b087605f
alex <tgh88@mail.ru>
2019-02-18 16:18:22 +0300

    add 'question-quantity' block

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,35 +3,27 @@ class UsersController < ApplicationController
     @users = [
       User.new(
       id: 1,
-      name: 'Alexey',
-      username: 'lucky',
-      avatar_url: 'https://pp.userapi.com/c849224/v849224224/7ee25/RHxvp-sdN38.jpg?ava=1'
-    ),
-    User.new(
-      id: 2,
-      name: 'Sasha',
-      username: 'kozlik')
-    ]
+      name: 'Ivan',
+      username: 'Petrov',
+      avatar_url: 'https://i.ytimg.com/vi/N5LssJweHg8/hqdefault.jpg')]
   end
 
   def new
   end
-
+  
   def edit
   end
 
   def show
     @user = User.new(
-      name: "Vasya",
-      username: 'vasya',
-      avatar_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTZ-J4K3bUmRbnVyopYQETMcSnIO2G1vmeTCGbgKs6hyO8xhLFVw')
+      name: 'Ivan',
+      username: 'Petrov',
+      avatar_url: 'https://i.ytimg.com/vi/N5LssJweHg8/hqdefault.jpg')
 
     @questions = [
       Question.new(text: 'Как дела?', created_at: Date.parse('15.02.2019')),
-      Question.new(
-        text: 'В чем смысл жизни?', created_at: Date.parse('15.02.2019')
-      )
-    ]
+      Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('15.02.2019')),
+      Question.new(text: 'Как дела?', created_at: Date.parse('15.02.2019'))]
 
     @new_question = Question.new
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def declension(number, vopros, voprosa, voprosov)
+    quantity = number % 100
+
+    return voprosov if quantity.between?(11, 14)
+
+    quantity = number % 10
+    case quantity
+    when 1 then return vopros
+    when 2..4 then return voprosa
+    when 5..9, 0 then return voprosov
+    end
+  end
 end
