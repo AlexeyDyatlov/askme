1d0d5cc67aa2cfa2af1dc14582fc8b5de5ea52bf
Alexey <tgh88@mail.ru>
2019-02-20 23:22:45 +0300

    gemfile update

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2019_02_14_101700) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
